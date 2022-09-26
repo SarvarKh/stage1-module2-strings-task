a8d437316e81e10dcdee67b042248f691c4a3ec9
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-26 14:47:51 +0500

    Solve failing last test

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,7 +1,6 @@
 package com.epam.mjc;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class StringSplitter {
 
@@ -12,7 +11,31 @@ public class StringSplitter {
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
+    public static void main(String args[]) {
+        String source = "qw3e1rt4yu2i3opa1sd1fg2hj4kl";
+        List<String> delimiters = List.of("1", "2", "3");
+        StringSplitter stringSplitter = new StringSplitter();
+        stringSplitter.splitByDelimiters(source, delimiters);
+//        ===>
+//          List.of("qw", "e", "rt4yu", "i", "opa", "sd", "fg", "hj4kl")
+    }
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        try {
+            StringBuilder all = new StringBuilder();
+            for (String d : delimiters){
+                all.append(d);
+            }
+
+            List<String> splitStringArr = new ArrayList<>();
+
+            StringTokenizer tokenizer = new StringTokenizer(source, all.toString());
+            while (tokenizer.hasMoreTokens()){
+                splitStringArr.add(tokenizer.nextToken());
+            }
+
+            return splitStringArr;
+        } catch (UnsupportedOperationException ex) {
+            throw new UnsupportedOperationException("You should implement this method." + ex.toString());
+        }
     }
 }
